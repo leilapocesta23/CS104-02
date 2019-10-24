f496a44cfd98f1934119b3004259d5fe2ab196df
leilapocesta23 <55250925+leilapocesta23@users.noreply.github.com>
2019-10-23 21:04:52 -0400

    Add files via upload

---
+++ names.list.py
@@ -0,0 +1,9 @@
+Names = ["Gio", "Jarrett", "Jayda", "Linda", "Tina"]
+N1 = input("enter first name")
+Search = input("enter search name:")
+
+if search in names:
+    print(search, "was found")
+else:
+    print(search, "was not found")
+    
